34ce19b19d2c95c9fbd520675e0695a5d28e7863
Wesley Fuchter <wesley.fuchter@gmail.com>
2019-10-09 19:56:53 -0400

    initial commit

---
+++ people-service/src/test/java/com/wesleyfuchter/peopleservice/NativePeopleResourceIT.java
@@ -0,0 +1,9 @@
+package com.wesleyfuchter.peopleservice;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativePeopleResourceIT extends PeopleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
